853b9200a3ee89cd877c8cf25051530414fb4302
Sonia Olivera <61550104+SoniaOlivera@users.noreply.github.com>
2024-02-07 23:18:29 -0300

    Add files via upload

---
+++ Components/recycle/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Powered by</p>
+        <img className='img' src='/images/DH.png' alt='logo'/>
+        
+
+    </footer>
+  )
+}
+export default Footer
